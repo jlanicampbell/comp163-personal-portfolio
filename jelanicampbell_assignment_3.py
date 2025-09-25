81a22a590c345c5f3d7a4bd285ae390cdbb987d6
Jelani Campbell <jelani.campbell56@gmail.com>
2025-09-24 23:13:45 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ jelanicampbell_assignment_3.py
@@ -0,0 +1,12 @@
+student_name = "Jelani Campbell"
+current_gpa = 3.2
+study_hours = 25
+social_points = 50
+stress_level = 50
+
+print("Welcome to Assignment 3!")
+print("Student Name:", student_name)
+print("Current GPA:", current_gpa)
+print("Study Hours:", study_hours)
+print("Social Points:", social_points)
+print("Stress level:", stress_level)
